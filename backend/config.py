3f4947ab78992458e2f933ff3f731af9d53866e3
swatantra29 <165281539+swatantra29@users.noreply.github.com>
2025-05-26 00:06:51 +0530

    Delete backend directory

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'niggaballz'
-    MYSQL_DB = 'logistics_db'
