f8b4e6c03607de75c0e485fd4cd1abe83692bf91
swatantra29 <165281539+swatantra29@users.noreply.github.com>
2025-11-02 08:47:14 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'niggaballz'
+    MYSQL_PASSWORD = 'your mysql root password'
     MYSQL_DB = 'logistics_db'
